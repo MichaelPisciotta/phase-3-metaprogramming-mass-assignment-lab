f2f2fe51afef20510f05f021cf5e0bc0e67fa6ab
MichaelPisciotta <mfp56@miami.edu>
2021-10-11 17:05:58 -0400

    need to work on with TC

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Person
-  # your code here
+
+attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(args)
+    args.each_key do |key, value|
+    self.send("#{key}=", hash[key])
+    @properties_count = 40
+  end
+
+end 
+
+
+avi_attributes = {
+  name: "Avi",
+  birthday: "01/29/1984",
+  hair_color: "brown",
+  eye_color: "brown",
+  height: "short",
+  weight: "good",
+  handed: "lefty",
+  complexion: "decent",
+  t_shirt_size: "medium",
+  wrist_size: "small",
+  glove_size: "normal",
+  pant_length: "32",
+  pant_width: "32"
+} 
+
+spencer_attributes = {
+        name: "Spencer",
+        hair_color: "N/A",
+        height: "medium",
+        weight: "good",
+        handed: "righty"
+      }
+
+
+
 end
